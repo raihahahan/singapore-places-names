345bcbd97c6e13668b04817c62ab8e774388e787
Muhd Raihan <muhdraihan1140@gmail.com>
2022-06-07 22:08:01 +0800

    removed unused imports

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,5 @@
 from scrapers.mainWikiScrape import getWikiData
-from helpers.constants import inputStr
-from helpers.cliHelpers import input_1, input_2, input_3, input_4
 from cli.cli import cli
-from scripts.helpers.cliHelpers import convertNumToList
 
 def main():
     soup=getWikiData()
